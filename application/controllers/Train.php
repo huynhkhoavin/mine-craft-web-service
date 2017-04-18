e3dbae38f3a969153df3ae7a8e1eaa8e5a8d02b1
Huỳnh Khoa Vin <khoavin@gmail.com>
2017-04-18 17:39:40 +0700

    create

---
+++ application/controllers/Train.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Train extends CI_Controller{
+    public function __construct(){
+        parent::__construct();
+    }
+    public function getallitem(){
+          $this->load->database();
+            $query = "CALL get_all_item()";
+            $result = $this->db->query($query);
+            echo json_encode(array('data'=>$result->result_array()));
+            $this->db->close();
+    }
+    public function getcomment(){
+        $item_id = $this->input->post('item_id');
+        $this->load->database();
+            $query = "CALL get_all_item_comment(?)";
+            $result = $this->db->query($query, array('p_item_id' => $item_id));
+            echo json_encode($result->result_array());
+            $this->db->close();
+    }
+}
