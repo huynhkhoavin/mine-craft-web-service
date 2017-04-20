ce2a8031c9011d558513180212d6889193adee4c
Huỳnh Khoa Vin <khoavin@gmail.com>
2017-04-20 14:27:56 +0700

    General

---
+++ application/controllers/General.php
@@ -0,0 +1,28 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class General extends CI_Controller{
+    public function __construct(){
+        parent::__construct();
+    }
+    public function get_all_item(){
+          $this->load->database();
+            $query = "CALL get_all_items()";
+            $result = $this->db->query($query);
+            echo json_encode(array('data'=>$result->result_array()));
+            $this->db->close();
+    }
+    public function get_all_categories(){
+        $this->load->database();
+            $query = "CALL get_all_categories()";
+            $result = $this->db->query($query);
+            echo json_encode(array('data'=>$result->result_array()));
+            $this->db->close();
+    }
+    public function get_all_item_type(){
+        $this->load->database();
+            $query = "CALL get_all_item_type()";
+            $result = $this->db->query($query);
+            echo json_encode(array('data'=>$result->result_array()));
+            $this->db->close();
+    }
+}
